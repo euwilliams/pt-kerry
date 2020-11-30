73692e8a32b6ccf21ca6ef5f8cd1f8ca7968e2b7
euwilliams <62066176+euwilliams@users.noreply.github.com>
2020-11-30 13:15:50 -0500

    finishing up

--- contact.php
+++ contact.php
@@ -4,30 +4,43 @@ $field_email = $_POST['email'];
 $field_message = $_POST['message'];
 
 
-$mail_to = 'euwilliams@gmail.com';
+$mail_to = 'pelvichealthptwv@gmail.com';
 $subject = '#message#' .$field_name;
+$subject = 'Message from '.$field_name;
 
 $body_message = 'From: ' .$field_name."\r\n";
 $body_message .= 'email: ' .$field_email."\r\n";
 $body_message .= 'message: ' .$field_message.;
+$body_message = 'From: '.$field_name."\r\n";
+$body_message .= 'E-mail: '.$field_email."\r\n";
+$body_message .= 'Message: '.$field_message;
 
 $headers = 'From: '.$email."rn";
 $headers .= 'Reply-To: '.$email."rn";
 
 $mail_status = mail($mail_to, $subject, $body_message, $headers)
+$mail_status = mail($mail_to, $subject, $body_message, $headers);
 
 if ($mail_status) { ?>
   <script language="javascript" type="text/javascript">
     alert('Thank you for contacting us, we will get back to you as soon as possible!');
     window.location = 'contact.html';
   </script>
+ <script language="javascript" type="text/javascript">
+  alert('Thank you for contacting us, we will get back to you as soon as possible!');
+  window.location = 'contact.html';
+ </script>
 <?php
 }
 else{ ?>
   <script language="javascript" type="text/javascript">
-  alert('Message sending failed. Please send an email to kwing17@gmail.com');
+  alert('Message sending failed. Please send an email to pelvichealthptwv@gmail.com');
+else { ?>
+ <script language="javascript" type="text/javascript">
+  alert('Message sending failed. Please, send an email to pelvichealthptwv@gmail.com');
   window.location = 'contact.html';
 </script>
+ </script>
 <?php
 }
 ?>
